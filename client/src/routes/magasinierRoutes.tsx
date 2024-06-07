27f79e06c34638f4c576172bba1f308d42f4f924
Abdelillah <abdelillah2003@gmail.com>
2024-06-07 21:18:29 +0100

    Update TopBar and SideBar colors

--- client/src/routes/magasinierRoutes.tsx
+++ client/src/routes/magasinierRoutes.tsx
@@ -7,155 +7,154 @@ import ChangelogPage from "../pages/changelog/ChangelogPage.tsx";
 import AnalyticsPage from "../pages/dashboard/AnalyticsPage.tsx";
 import SaasPage from "../pages/dashboard/SaasPage.tsx";
 import DocumentationPage from "../pages/documentation/DocumentationPage.tsx";
-import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
- //icons
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import AccountTreeIcon from '@mui/icons-material/AccountTree';
-import PersonIcon from '@mui/icons-material/Person';
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
-import AccessibilityIcon from '@mui/icons-material/Accessibility';
-import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
+//icons
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import PersonIcon from "@mui/icons-material/Person";
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
 
 //MAGASINIER
-import  Profile  from "../pages/profile/Profile.tsx";
-import StockManagement from "../magasinier/stockManagement.tsx"
-import BRManagement from "../magasinier/brManagement.tsx"
-import RapportInventaire from "../magasinier/rapportInventaire.tsx"
-import BDRManagement  from "../magasinier/bdrManagement.tsx";
+import Profile from "../pages/profile/Profile.tsx";
+import StockManagement from "../magasinier/stockManagement.tsx";
+import BRManagement from "../magasinier/brManagement.tsx";
+import RapportInventaire from "../magasinier/rapportInventaire.tsx";
+import BDRManagement from "../magasinier/bdrManagement.tsx";
 import BSManagement from "../magasinier/bsManagement.tsx";
 import BDManagement from "../magasinier/bdManagement.tsx";
 import React from "react";
 export const magasinierRoutes: RouteType[] = [
-    {
-      index: true,
-      element: <HomePage />,
-      state: "home"
+  {
+    index: true,
+    element: <HomePage />,
+    state: "home",
+  },
+
+  {
+    path: "/dashboard",
+    element: <DashboardPageLayout />,
+    state: "dashboard",
+    sidebarProps: {
+      displayText: "Dashboard",
+      icon: <DashboardOutlinedIcon />,
     },
-  
-    {
-      path: "/dashboard",
-      element: <DashboardPageLayout />,
-      state: "dashboard",
-      sidebarProps: {
-        displayText: "Dashboard",
-        icon: <DashboardOutlinedIcon />
+    child: [
+      {
+        index: true,
+        element: <DashboardIndex />,
+        state: "dashboard.index",
       },
-      child: [
-        {
-          index: true,
-          element: <DashboardIndex />,
-          state: "dashboard.index"
+      {
+        path: "/dashboard/default",
+        element: <DefaultPage />,
+        state: "dashboard.default",
+        sidebarProps: {
+          displayText: "Default",
         },
-        {
-          path: "/dashboard/default",
-          element: <DefaultPage />,
-          state: "dashboard.default",
-          sidebarProps: {
-            displayText: "Default"
-          },
-        },
-        {
-          path: "/dashboard/analytics",
-          element: <AnalyticsPage />,
-          state: "dashboard.analytics",
-          sidebarProps: {
-            displayText: "Analytic"
-          }
-        },
-        {
-          path: "/dashboard/saas",
-          element: <SaasPage />,
-          state: "dashboard.saas",
-          sidebarProps: {
-            displayText: "Saas"
-          }
-        }
-      ]
-    },
-    {
-      path: "/stock",
-      element: <StockManagement />,
-      state: "stock",
-      sidebarProps: {
-        displayText: "Stock Management",
-        icon: <ManageAccountsIcon />
       },
-      child: [
-        {
-          path: "/stock/rapport",
-          element: <RapportInventaire />,
-          state: "stock.rapport",
-          sidebarProps: {
-            displayText: " Inventory Rapports"
-          },
-        }
-      ]
+      {
+        path: "/dashboard/analytics",
+        element: <AnalyticsPage />,
+        state: "dashboard.analytics",
+        sidebarProps: {
+          displayText: "Analytic",
+        },
+      },
+      {
+        path: "/dashboard/saas",
+        element: <SaasPage />,
+        state: "dashboard.saas",
+        sidebarProps: {
+          displayText: "Saas",
+        },
+      },
+    ],
+  },
+  {
+    path: "/stock",
+    element: <StockManagement />,
+    state: "stock",
+    sidebarProps: {
+      displayText: "Stock Management",
+      icon: <ManageAccountsIcon />,
     },
-    {
-      path: "/br",
-      element: <BRManagement />,
-      state: "br",
-      sidebarProps: {
-        displayText: "Bon de Laivraison",
-        icon: <AccountTreeIcon />
-      }
+    child: [
+      {
+        path: "/stock/rapport",
+        element: <RapportInventaire />,
+        state: "stock.rapport",
+        sidebarProps: {
+          displayText: " Inventory Rapports",
+        },
+      },
+    ],
+  },
+  {
+    path: "/br",
+    element: <BRManagement />,
+    state: "br",
+    sidebarProps: {
+      displayText: "Bon de Laivraison",
+      icon: <AccountTreeIcon />,
     },
-    {
-      path: "/bs",
-      element: <BSManagement />,
-      state: "bs",
-      sidebarProps: {
-        displayText: "Bon de Sortie",
-        icon: <AccessibilityIcon />
-      }
-    }
-    ,
-    {
-      path: "/bd",
-      element: <BDManagement />,
-      state: "bd",
-      sidebarProps: {
-        displayText: "Bon de Décharge",
-        icon: <AccessibilityIcon />
-      }
+  },
+  {
+    path: "/bs",
+    element: <BSManagement />,
+    state: "bs",
+    sidebarProps: {
+      displayText: "Bon de Sortie",
+      icon: <AccessibilityIcon />,
     },
-    
-    {
-      path: "/bdr",
-      element: <BDRManagement />,
-      state: "bdr",
-      sidebarProps: {
-        displayText: "Bon de Décharge de Ristitution",
-        icon: <AccessibilityIcon />
-      }
+  },
+  {
+    path: "/bd",
+    element: <BDManagement />,
+    state: "bd",
+    sidebarProps: {
+      displayText: "Bon de Décharge",
+      icon: <AccessibilityIcon />,
     },
-    {
-      path: "/profile",
-      element: <Profile />,
-      state: "profile",
-      sidebarProps: {
-        displayText: "Profile Settings",
-        icon: <PersonIcon />
-      }
+  },
+
+  {
+    path: "/bdr",
+    element: <BDRManagement />,
+    state: "bdr",
+    sidebarProps: {
+      displayText: "Bon de Décharge de Ristitution",
+      icon: <AccessibilityIcon />,
     },
-    {
-      path: "/documentation",
-      element: <DocumentationPage />,
-      state: "documentation",
-      sidebarProps: {
-        displayText: "Documentation",
-        icon: <ArticleOutlinedIcon />
-      }
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+    state: "profile",
+    sidebarProps: {
+      displayText: "Profile Settings",
+      icon: <PersonIcon />,
     },
-    {
-      path: "/changelog",
-      element: <ChangelogPage />,
-      state: "changelog",
-      sidebarProps: {
-        displayText: "Changelog",
-        icon: <FormatListBulletedOutlinedIcon />
-      }
-    }
-  ];
-  
-  export default magasinierRoutes ;
+  },
+  {
+    path: "/documentation",
+    element: <DocumentationPage />,
+    state: "documentation",
+    sidebarProps: {
+      displayText: "Documentation",
+      icon: <ArticleOutlinedIcon />,
+    },
+  },
+  {
+    path: "/changelog",
+    element: <ChangelogPage />,
+    state: "changelog",
+    sidebarProps: {
+      displayText: "Changelog",
+      icon: <FormatListBulletedOutlinedIcon />,
+    },
+  },
+];
+
+export default magasinierRoutes;
